634c77a9388ac7adb9a40b51047eda8a83d377ea
Tim Scheurer <tim.scheurer1@gmail.com>
2023-01-11 20:11:39 +0100

    Add sdcv mode and configure for use with agda.

--- init.el
+++ init.el
@@ -74,3 +74,15 @@
       (unless (server-running-p)
         (message "Starting a server...")
         (server-start)))))
+
+;; agda/plfa specific settings
+
+;; auto-load agda-mode for .agda and .lagda.md
+;;(setq auto-mode-alist
+;;   (append
+;;     '(("\\.agda\\'" . agda2-mode)
+;;       ("\\.lagda.md\\'" . agda2-mode))
+;;     auto-mode-alist))
+
+;;(load-file (let ((coding-system-for-read 'utf-8))
+;;                (shell-command-to-string "agda-mode locate")))
